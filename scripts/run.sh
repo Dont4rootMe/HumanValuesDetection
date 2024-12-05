4219297671cbce9b5fef825653eae12c83c57d10
Artem Fedorov <s02210577@gse.cs.msu.ru>
2024-12-05 21:36:49 +0300

    update on building container + adding readme

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,22 +1,6 @@
-helpFunction()
-{
-   echo ""
-   echo "Usage: $0 -m path to model"
-   echo -e "\t-m determine path to modules with model apis"
-   exit 1 # Exit script after printing help
-}
-
-while getopts "d:m:" opt
-do
-   case "$opt" in
-      m ) model_api="$OPTARG" ;;
-      ? ) helpFunction ;; # Print helpFunction in case parameter is non-existent
-   esac
-done
-
 docker run -it --rm \
         -p 8000:8000 \
-        -v "$model_api:/models:ro" \
+        -v "$PWD/models:/models:ro" \
         --platform linux/amd64 \
         --name model_backend model_backend-img \
         /bin/bash  -c 'for req in /models/*/model/*.txt; do pip install -r $req; done; python /src/api.py'
--- src/api.py
+++ src/api.py
@@ -1,5 +1,6 @@
 import uvicorn
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from fastapi import FastAPI
 from schemes import MarkupResponse
 import importlib
@@ -11,6 +12,7 @@ import sys
 models = {}
 sys.path.append(dirname('/model'))
 for model_path in os.listdir('/models'):
+    print(model_path)
     spec = importlib.util.spec_from_file_location("model_api", f"/models/{model_path}/model/model_api.py")
     module = importlib.util.module_from_spec(spec)
     spec.loader.exec_module(module)
@@ -28,11 +30,14 @@ app.add_middleware(
 )
 
 
-@app.get('/predict/human-values')
+@app.post('/predict/human-values', response_model=MarkupResponse)
 async def train_on_syntetic_dataset(text: str, model: str = 'gliner'):
-    from fastapi.responses import JSONResponse
-    response = models[model]('23 февраля в войсковой части 5526 прошло чествование воспитанников военно-патриотического клуба «Крепость». Заместитель командира части по идеологической работе майор Олег Ляшук отметил, что воспитание подрастающего поколения в патриотическом ключе, в стремлении к здоровому образу жизни, уважению к традициям, культурным ценностям и исторической памяти государства является главным профилактическим фактором безнравственности и аморальности. «Вместе мы вносим огромный вклад в будущее страны и нравственное здоровье нашего общества. Служба Родине во все времена была почетна. А служить можно по-разному, и не обязательно с оружием в руках. Служить можно в любом возрасте, служить можно и парню, и девушке. Служить можно и словом, и делом!» – подытожил Олег Ляшук.')
+    response = models[model](text)
     return JSONResponse(content=response)
+
+@app.get('available_models')
+async def get_available_models():
+    return models.keys()
 
 
 if __name__ == '__main__':
